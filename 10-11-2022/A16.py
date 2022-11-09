3c21cdd885fbd5e68a90359352c16dd44fecc32e
Animesh Murmu <am2646374@gmail.com>
2022-11-09 21:15:46 +0530

    Wed Nov  9 21:15:46 IST 2022

--- 10-11-2022/A16.py
+++ 10-11-2022/A16.py
@@ -1,6 +1,6 @@
 # 1.WAP to take a list and sum the number from the list.
 
-l = [1,2,3,4,5,6,7,8,9,0]
+l = [1,2,3,4,5,6,7,8,9,10]
 s = 0
 for i in l:
     s = s + i
--- 10-11-2022/A20.py
+++ 10-11-2022/A20.py
@@ -1,5 +1,5 @@
 # 5.WAP to take list of 10 number and store even position number in saparate list and odd position number in saparate list and display the two list.
-l = [1,2,3,4,5,6,7,8,9]
+l = [1,2,3,4,5,6,7,8,9, 10]
 e, o = [],[]
 
 for i, j in enumerate(l):
